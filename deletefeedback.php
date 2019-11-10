e58b0345d1049415319ed47fc503b9c7775965cf
vivekAgarwalcs <55972371+vivekAgarwalcs@users.noreply.github.com>
2019-11-10 17:57:28 +0530

    Add files via upload

---
+++ deletefeedback.php
@@ -0,0 +1,34 @@
+<?php
+
+
+
+
+
+require "connection.php";
+
+
+$fid = $_REQUEST["f_id"];
+
+
+
+$userQuery =  "SELECT  *  FROM `feedback` WHERE `fid` = $fid;";
+
+$getFileResult = mysqli_query($connection,$userQuery );
+
+
+if($getFileResult){
+		$feQuery = "DELETE FROM `feedback` WHERE `fid` ='$fid'";
+		
+		
+  
+  mysqli_query($connection,$feQuery);
+	
+header("location: feedback.php");
+	
+	
+}
+ else {
+
+    die("ERROR: Only the user who created the file can delete it.");
+
+}?>
---
+++ resturant.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 23, 2019 at 03:34 AM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `resturant`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tables`
+--
+
+CREATE TABLE `tables` (
+  `tid` int(11) NOT NULL,
+  `tname` varchar(50) NOT NULL,
+  `status` tinyint(10) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tables`
+--
+
+INSERT INTO `tables` (`tid`, `tname`, `status`) VALUES
+(1, 'table 1', 1),
+(2, 'table 2', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `uname` varchar(200) NOT NULL,
+  `address` varchar(200) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `userType` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `uname`, `address`, `phone`, `email`, `password`, `userType`) VALUES
+(1, 'vivek agarwal', 'Gla university, 17km Stone, NH-2, Mathura-Delhi Road P.O. Chaumuhan, Mathura', '08533094283', 'vivek@google.com', '1234', 1),
+(7, 'vivek agarwal', 'Gla university, 17km Stone, NH-2, Mathura-Delhi Road P.O. Chaumuhan, Mathura', '08533094283', 'gla@gmail.com', '1234', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `utid` int(20) NOT NULL,
+  `uid` int(50) NOT NULL,
+  `tname` varchar(50) NOT NULL,
+  `uname` varchar(50) NOT NULL,
+  `person` int(10) NOT NULL,
+  `time` varchar(50) NOT NULL,
+  `date` date NOT NULL,
+  `booktime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `request` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_table`
+--
+
+INSERT INTO `user_table` (`utid`, `uid`, `tname`, `uname`, `person`, `time`, `date`, `booktime`, `phone`, `email`, `request`) VALUES
+(11, 1, 'table 2', 'vivek agarwal', 1, '12PM-1PM', '2019-10-12', '2019-10-22 18:48:36', '08533094283', 'vivek@google.com', '	no				'),
+(12, 1, 'table 1', 'vivek agarwal', 1, '12PM-1PM', '2019-10-01', '2019-10-22 18:49:25', '08533094283', 'vivek@google.com', '	no				'),
+(13, 1, 'table 1', 'vivek agarwal', 1, '12PM-1PM', '2019-11-01', '2019-10-22 18:51:39', '08533094283', 'vivek@google.com', '	no				'),
+(15, 1, 'table 2', 'vivek agarwal', 2, '3PM-4PM', '2019-10-16', '2019-10-23 01:19:47', '08533094283', 'vivek@google.com', '	viev\r\n\r\n				'),
+(16, 1, 'table 1', 'vivek agarwal', 1, '12PM-1PM', '2019-10-08', '2019-10-23 01:31:13', '08533094283', 'vivek@google.com', '					');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tables`
+--
+ALTER TABLE `tables`
+  ADD PRIMARY KEY (`tid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`utid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tables`
+--
+ALTER TABLE `tables`
+  MODIFY `tid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `utid` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
